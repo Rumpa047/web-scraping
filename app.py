87265ca7e2433e5b7d9eb06a7a05461bb46a3ccf
Rumpa047 <47315020+Rumpa047@users.noreply.github.com>
2019-02-05 11:26:06 +0600

    2nd commit
    Inspecting the Response

--- app.py
+++ app.py
@@ -21,8 +21,15 @@ r = requests.get("http://example.com/page", params=dict(
     page=2
 ))
 
-# # See what response code the server sent back (useful for detecting 4XX or 5XX errors)
-# print(r.status_code)
+# See what response code the server sent back (useful for detecting 4XX or 5XX errors)
+print(r.status_code)
 
-# # Access the full response as text (get the HTML of the page in a big string)
+# Access the full response as text (get the HTML of the page in a big string)
 # print(r.text)
+
+# Look for a specific substring of text within the response
+if "blocked" in r.text:
+    print("we've been blocked")
+
+# Check the response’s Content Type (see if you got back HTML, JSON, XML, etc)
+print(r.headers.get("content-type", "unknown"))
