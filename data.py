58843d85add19b1d9bb1f8e79f8dc4628e937442
Rumpa047 <47315020+Rumpa047@users.noreply.github.com>
2019-02-05 19:06:43 +0600

    2nd commit
    use selector

--- data.py
+++ data.py
@@ -5,9 +5,15 @@ r = requests.get("http://www.english-bangla.com/browse/index/a")
 print(r.status_code)
 print(r.headers.get("content-type", "unknown"))
 soup = BeautifulSoup(r.content, "lxml")
-print(soup)
+print(type(soup))
 print(soup.title)
 
+# tags = soup.find("div", id="cat_page").find("ul").find_all('a')
+# for i in tags:
+#     print(i)
+
+for i in soup.select('ul'):
+    print(i.text)
 # print(soup.find_all('a')['href'])
 # tags = soup.find_all("li")
 # print(tags)
