22f6a661a9771d2d195804f53bda11846d50ef0f
Rumpa047 <47315020+Rumpa047@users.noreply.github.com>
2019-02-05 11:17:54 +0600

    1st commit
    Making Simple Requests

---
+++ app.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# Make a simple GET request (just fetching a page)
+
+r = requests.get("http://example.com/page")
+
+
+# Make a POST requests (usually used when sending information to the server like submitting a form)
+
+r = requests.post("http://example.com/page", data=dict(
+    email="me@domain.com",
+    password="secret_value"
+))
+
+# Pass query arguments aka URL parameters (usually used when making a search query or paging through results)
+
+r = requests.get("http://example.com/page", params=dict(
+    query="web scraping",
+    page=2
+))
+
+# # See what response code the server sent back (useful for detecting 4XX or 5XX errors)
+# print(r.status_code)
+
+# # Access the full response as text (get the HTML of the page in a big string)
+# print(r.text)
