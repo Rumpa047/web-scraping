092b848491db8085bd7e759190893e4469e96a6f
Rumpa047 <47315020+Rumpa047@users.noreply.github.com>
2019-02-18 11:52:37 +0600

    1st commit
    Show first page word and meaning of A to Z letter

--- data.py
+++ data.py
@@ -1,19 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("http://www.english-bangla.com/browse/index/a")
+r = requests.get("http://www.english-bangla.com/browse")
 print(r.status_code)
-print(r.headers.get("content-type", "unknown"))
 soup = BeautifulSoup(r.content, "lxml")
-print(type(soup))
-print(soup.title)
+# print(type(soup))
+# print(soup.title)
+alphabt_link = soup.find("div", id="cat_page").find("div", class_="a-z").find_all('a')
+# print(alphabt_link)
+for letter in alphabt_link:
+    letter_link =letter.get('href')
+    print(letter_link)
+    letter_link_req = requests.get(letter_link)
+    letter_link_soup = BeautifulSoup(letter_link_req.content, "lxml")
+    letter = letter_link_soup.find("div", id="cat_page").find("ul").find_all('a')
+    for i in letter:
+        print(i.text)
+        letter_meaning_link = i.get('href')
+        letter_meaning_link_req = requests.get(letter_meaning_link)
+        letter_meaning_soup = BeautifulSoup(letter_meaning_link_req.content, "lxml")
+        try:
+            letter_meaning = letter_meaning_soup.find("div", id="w_info").find("span", class_="format1").text
+            print(letter_meaning)
+        except:
+            print("Not a valid word")
 
-# tags = soup.find("div", id="cat_page").find("ul").find_all('a')
-# for i in tags:
-#     print(i)
-
-for i in soup.select('ul'):
-    print(i.text)
+# for i in soup.select('ul'):
+#     print(i.text)
 # print(soup.find_all('a')['href'])
 # tags = soup.find_all("li")
 # print(tags)
