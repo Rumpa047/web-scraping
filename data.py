3d65ebb6d4ea3cc436a4c6a283ab997a0663acfb
Rumpa047 <47315020+Rumpa047@users.noreply.github.com>
2019-02-18 18:23:23 +0600

    2nd commit
    scrape data from dictionary

--- data.py
+++ data.py
@@ -4,10 +4,8 @@ from bs4 import BeautifulSoup
 r = requests.get("http://www.english-bangla.com/browse")
 print(r.status_code)
 soup = BeautifulSoup(r.content, "lxml")
-# print(type(soup))
-# print(soup.title)
+
 alphabt_link = soup.find("div", id="cat_page").find("div", class_="a-z").find_all('a')
-# print(alphabt_link)
 for letter in alphabt_link:
     letter_link =letter.get('href')
     print(letter_link)
@@ -24,9 +22,34 @@ for letter in alphabt_link:
             print(letter_meaning)
         except:
             print("Not a valid word")
+    try:
+        next_page_link = letter_link_soup.find("div", class_="pagination").find('a')
+    except:
+        next_page_link = None
 
-# for i in soup.select('ul'):
-#     print(i.text)
-# print(soup.find_all('a')['href'])
-# tags = soup.find_all("li")
-# print(tags)
+    while 1:
+        if next_page_link == None:
+            break
+        else:
+            next_page_req = requests.get(next_page_link.get('href'))
+            next_page_soup = BeautifulSoup(next_page_req.content, "lxml")
+            letter = next_page_soup.find("div", id="cat_page").find("ul").find_all('a')
+            for i in letter:
+                print(i.text)
+                letter_meaning_link = i.get('href')
+                letter_meaning_link_req = requests.get(letter_meaning_link)
+                letter_meaning_soup = BeautifulSoup(letter_meaning_link_req.content, "lxml")
+                try:
+                    letter_meaning = letter_meaning_soup.find("div", id="w_info").find("span", class_="format1").text
+                    print(letter_meaning)
+                except:
+                    print("Not a valid word")
+
+            try:
+                pagination_links = next_page_soup.find("div", class_="pagination").find_all('a')
+                for i in pagination_links:
+                    if i.text == "Next >":
+                        next_page_link = i
+                        break
+            except:
+                next_page_link = None
